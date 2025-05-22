51a0b210febc4706c071d2ae2aa40fdb743aa9a5
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-05-21 22:25:39 -0400

    almost done

--- index.js
+++ index.js
@@ -1,55 +1,55 @@
 const title = document.getElementById('title');
-const form = document.getElementById('form');
-const list = document.getElementById('list');
-const input = document.getElementById('input');
-//create a task element
-console.log(list.firstChild);
-console.log(input.parentNode);
+const todoForm = document.getElementById('todoForm');
+const todoList = document.getElementById('todoList');
+const todoInput = document.getElementById('todoInput');
+
+
+console.log(todoList.firstChild);
+console.log(todoInput.parentNode);
 
 //function to create a task
-function createTask(text){
-const task = doxument.createElement('li');
-task.textContext = text;
-list.appendChild(task);
+function createTask(taskText){
+const taskItem = document.createElement('li');
+taskItem.textContext = taskText;
+todoList.appendChild(taskItem);
 
 
 //clone to reuse the task
-const clone = task.clonenode(true);
-clone.textContent = text + " (Copy)";
-list.appendChild(clone);
+const clone = taskItem.cloneNode(true);
+clone.textContent = taskText + " (Copy)";
+todoList.appendChild(clone);
 
 //first event-listener to modify the text content
-task.addEventListener('click', () => {
-    task.textContent = "Done: " + task.textContent;
-    task.classList.toggle('Done');
+taskItem.addEventListener('click', () => {
+    taskItem.textContent = "Done: " + taskItem.textContent;
+    taskItem.classList.toggle('Done');
 });
-task.style.cursor = 'pointer';
-task.setAttribute('title','click to mark as done');
-return task;
+taskItem.style.cursor = 'pointer';
+taskItem.setAttribute('title','click to mark as done');
+return taskItem;
 }
 
 //second fot form
-form.addEventListener('submit',(e) => {
+todoForm.addEventListener('submit',(e) => {
     e.preventDefault();
-    if(input.value.trim() !== ''){
-        createTask(input.value);
-        input.value = " ";
+    if(todoInput.value.trim() !== ''){
+        createTask(todoInput.value);
+        todoInput.value = " ";
 
     }else{
         alert('Please enter a valid task!');
     }
 });
-
+console.log('Screen Width: ${window.innerWidth}');
 alert('Welcome to the To-Do App!');
 
 //form validatiopn with DOM
-
-input.addEventListener('inputs', () => {
-    if (input.value.length < 3) {
-        input.setCustomValidity('Task must be atleast 3 characters long.');
+todoInput.addEventListener('input', () => {
+    if (todoInput.value.length < 3) {
+        todoInput.setCustomValidity('Task must be atleast 3 characters long.');
     }
     else{
-        input.setCustomValidity('');
+        todoInput.setCustomValidity('');
     }
     
 });
