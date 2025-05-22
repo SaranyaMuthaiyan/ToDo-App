b386d015cdc080b5fb7e6dd6c6af6e5974d0b80d
Saranya Muthaiyan <saramuthaiyan5@gmail.com>
2025-05-21 21:07:59 -0400

    trialing the methods

--- README.md
+++ README.md
@@ -3,4 +3,12 @@ Objective:
 To create a web page , write a html code that contains a title,header, a form with input type text, submit button and another update button elements.
 Add the style sheet and js in html.
 Add ids to repective elements that needs to be called in js.
-Add the properties to the body and button in the stylesheet.
+Add the properties to the elements in the stylesheet.
+create a js file and add the cache elements.
+then create a task function and clone node to reuse the task emplate.
+Change the textcontent on 'click'
+Change the classlist properties and attributes
+Add the form submission using eventlistener
+adding BOM properties
+Validating the form with DOM event
+Change the title on button click
--- index.js
+++ index.js
@@ -1,3 +1,61 @@
-const title = document.getElementById('title')
+const title = document.getElementById('title');
 const form = document.getElementById('form');
-const input = document.getElementById('input')
+const list = document.getElementById('list');
+const input = document.getElementById('input');
+//create a task element
+console.log(list.firstChild);
+console.log(input.parentNode);
+
+//function to create a task
+function createTask(text){
+const task = doxument.createElement('li');
+task.textContext = text;
+list.appendChild(task);
+
+
+//clone to reuse the task
+const clone = task.clonenode(true);
+clone.textContent = text + " (Copy)";
+list.appendChild(clone);
+
+//first event-listener to modify the text content
+task.addEventListener('click', () => {
+    task.textContent = "Done: " + task.textContent;
+    task.classList.toggle('Done');
+});
+task.style.cursor = 'pointer';
+task.setAttribute('title','click to mark as done');
+return task;
+}
+
+//second fot form
+form.addEventListener('submit',(e) => {
+    e.preventDefault();
+    if(input.value.trim() !== ''){
+        createTask(input.value);
+        input.value = " ";
+
+    }else{
+        alert('Please enter a valid task!');
+    }
+});
+
+alert('Welcome to the To-Do App!');
+
+//form validatiopn with DOM
+
+input.addEventListener('inputs', () => {
+    if (input.value.length < 3) {
+        input.setCustomValidity('Task must be atleast 3 characters long.');
+    }
+    else{
+        input.setCustomValidity('');
+    }
+    
+});
+
+//update title on click
+const updateBtn = document.querySelector('#updateTitle');
+updateBtn.addEventListener('click', () => {
+    title.textContent = "Updated To-Do List";
+});
